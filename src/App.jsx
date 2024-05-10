d1c22064add80f63f84ccf987dd525670a215b9d
Zuhi732 <jeimejuhi008@gmail.com>
2024-05-10 21:07:14 +0800

    added firebase Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { RouterProvider } from 'react-router-dom'
-import './App.css'
-import router from './Componants/Routes/Routes'
+import { Toaster } from "react-hot-toast";
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import router from "./Componants/Routes/Routes";
 
 function App() {
-  
-
   return (
     <>
-     <RouterProvider router={router} />
+      <RouterProvider router={router} />
+      <Toaster />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
