7f605b56efc1edf7b81ec89d647232e206e1d394
Obyedullahilmamun <obyedullahilmamun@gmail.com>
2024-05-11 13:31:05 +0600

    checking my github pr

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { RouterProvider } from 'react-router-dom'
-import './App.css'
-import router from './Componants/Routes/Routes'
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import router from "./Componants/Routes/Routes";
 
 function App() {
-  
-
   return (
     <>
-     <RouterProvider router={router} />
+      <RouterProvider router={router} />
+      <div>checking github pr</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
