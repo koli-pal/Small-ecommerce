3e6f862fdfad1cf65272e1431d2d8119115bf991
unknown <Tamal Chakraborty>
2024-05-13 16:09:44 +0600

    Fix Dashboard Redirect

--- src/Pages/Customer/Dashboard/Dashboard.jsx
+++ src/Pages/Customer/Dashboard/Dashboard.jsx
@@ -1,8 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import CustomerNavbar from "../../../Componants/Customer/CustomerNavber";
+import { useEffect } from "react";
 
 export default function Dashboard() {
+  const navigate = useNavigate()
+  useEffect(()=>{navigate('/dashboard/orders')},[])
   return (
+
     <div>
       <CustomerNavbar />
       <div className="bg-gray-100 min-h-[90vh] ">
